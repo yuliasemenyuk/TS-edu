cf2ea06b4a03ccc1c5f7acb036bdc66c3f122ac5
yuliasemenyuk <semenyukyp@gmail.com>
2024-07-12 19:33:49 +0300

    private and public

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 class Department {
   name: string;
+  // only accesssible via method
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,14 +10,33 @@ class Department {
   describe(this: Department) {
     console.log("Department:" + this.name)
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees)
+  }
 };
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Lala');
+
+// Not possible to add this way 'cause of 'private'
+// accounting.employees[2] = "Anna"
+
+//While for name it's still possible
+accounting.name = "New Name"
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
 console.log(accounting);
 
-const accountingCopy = {name: "copy", describe: accounting.describe};
+// const accountingCopy = {name: "copy", describe: accounting.describe};
 
-accountingCopy.describe();
+// accountingCopy.describe();
