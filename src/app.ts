96b2a5d168644bfba5cd315348ab6cca8c4aeb90
yuliasemenyuk <semenyukyp@gmail.com>
2024-08-21 23:43:49 +0300

    readonly and shorthand initialization

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 class Department {
-  name: string;
+  // private id: string;
+  // private name: string;
   // only accesssible via method
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    //this.id = 'd1';
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department:" + this.name)
+    console.log(`Department (${this.id}): ${this.name}`)
   }
 
   addEmployee(employee: string) {
+    //Cannot assign to 'id' because it is a read-only property
+    // this.id ="d2"
     this.employees.push(employee);
   }
 
@@ -21,7 +25,7 @@ class Department {
   }
 };
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1","Accounting");
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Lala');
