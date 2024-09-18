dde7197c7085fadc3c56212cc9631ede808d74f3
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-18 23:32:34 +0300

    interface as function type

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,40 @@
+// Interface as a function type, both expressions works the same 
+
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-    readonly name: string;
+  readonly name: string;
 }
 interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
-interface Another {
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-}
+  constructor(n: string) {
+    this.name = n;
+  }
 
-class Person implements Greetable, Another {
-    name: string;
-    age = 30;
-
-    constructor(n: string) {
-        this.name = n;
-    }
-
-    greet(phrase: string): void {
-        console.log(phrase + " " + this.name);
-    }
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  }
 }
 
 let user1: Greetable;
 // let user1: Person;
 
-user1 = new Person ("Max");
+user1 = new Person("Max");
 // user1.name = "Andrew" - doesn't work due to "readonly" in interface
 
 user1.greet("Hi there, I'm");
