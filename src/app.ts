40e0b8c707b8f477767bc0bfd9472b8d6cca83ab
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-24 13:18:46 +0300

    generic function

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-const names: Array<string> = ["Max", "James"]; //string[]
-// names[0].split(' ');
+// const names: Array<string> = ["Max", "James"]; //string[]
+// // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Done");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Done");
+//   }, 2000);
+// });
 
-promise.then(data => {
-    data.split(" ")
-})
+// promise.then(data => {
+//     data.split(" ")
+// })
+
+function merge<T extends {}, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+console.log(merge({name: 'Max'}, {age: 30}));
+
+const result = merge({name: 'Max'}, {age: 30});
+const result2 = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
+
+console.log(result.age);
+console.log(result2.hobbies);
