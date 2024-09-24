71252567d7f5800bd235ff3a77202d8358b2880f
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-24 22:01:58 +0300

    generic func example

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,30 @@
 // })
 
 function merge<T extends {}, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA, objB);
+  return Object.assign(objA, objB);
 }
 
-console.log(merge({name: 'Max'}, {age: 30}));
+console.log(merge({ name: "Max" }, { age: 30 }));
 
-const result = merge({name: 'Max'}, {age: 30});
-const result2 = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
+const result = merge({ name: "Max" }, { age: 30 });
+const result2 = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
 console.log(result.age);
 console.log(result2);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(el: T): [T, string] {
+  let descriptionText = "Got no value.";
+
+  if (el.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (el.length > 1) {
+    descriptionText = "Got " + el.length + " elements";
+  }
+  return [el, descriptionText];
+}
+
+console.log(countAndDescribe(["Hi there", ""]));
