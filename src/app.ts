6d1a0a0711f328a8c46a512f4086027460a60e39
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-27 00:08:19 +0300

    Adding Multiple Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 //Decorator factory
 //Now we can accept arguments - logString that will be reurned by inner decorator function
 function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
   return function (target: Function) {
     console.log(logString);
     console.log(target);
@@ -8,7 +9,9 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
   return function (constructor: any) {
+    console.log("Rendering template");
     const hoohkEl = document.getElementById(hookId);
     const p = new constructor();
     if (hoohkEl) {
@@ -18,8 +21,8 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-//Now we should call decorator - () and it will return a decorator function
-// @Logger("LOGGING - PERSON")
+//Few decorators functions execute from bottom to up
+@Logger("LOGGING...")
 @WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
