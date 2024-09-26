1e935c82071bb21c94c753cdbeb7906df428a387
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-27 00:18:56 +0300

    Accessor & Parameter Decorators

--- src/app.ts
+++ src/app.ts
@@ -34,3 +34,32 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+//----------------------
+function Log(target: any, propertyName: string | Symbol) {
+    console.log("Property decorator")
+    console.log(target, propertyName)
+}
+class Product {
+    //decorator function executes when class definition is registered
+    @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive number");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
