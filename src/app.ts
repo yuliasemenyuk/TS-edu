f02b153a1c7042d2faf8809fb3534341513b841e
yuliasemenyuk <semenyukyp@gmail.com>
2024-09-29 21:57:20 +0300

    Validation with Decorators

--- src/app.ts
+++ src/app.ts
@@ -145,7 +145,79 @@ const button = document.querySelector("button")!;
 //we just call p.showMessage, so on click we've got undefined
 
 //But with @Autobind on showMessage method it's fixed
-button.addEventListener('click', p.showMessage);
+button.addEventListener("click", p.showMessage);
 
 //Vanilla JS OPTION TO FIX:
 // button.addEventListener('click', p.showMessage.bind(p));
+
+//--------------------------------
+const registeredValidators: ValidatorConfig = {};
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again");
+    return;
+  }
+  console.log(createdCourse);
+});
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["required"],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["positive"],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
